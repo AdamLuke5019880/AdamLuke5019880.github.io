121a7ea92f500e6e73e1c10c80c1d22a081c967d
AdamLuke5019880 <al5019880@lpssonline.com>
2026-04-01 12:38:41 +0000

    todo 6 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -62,7 +62,7 @@ var init = function (window) {
             game.checkCirclePosition(circles[2])
             game.checkCirclePosition(circles[3])
             game.checkCirclePosition(circles[4])
-            ]
+            
 
                        // TODO 9 : Iterate over the array
            
@@ -79,11 +79,21 @@ var init = function (window) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
                       
+                      if(circle.x > canvas.width){
+  circle.x = 0
+}
+
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            else if(circle.x < 0) {
+                circle.x = canvas.width;
+            } else if(circle.y > canvas.height){
+                circle.y = 0
+            } else if(circle.y < 0){
+                circle.y = canvas.height
+            }
            
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
-        }
+         }
         
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
